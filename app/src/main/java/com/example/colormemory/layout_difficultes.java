3c97ce494f9a8fd239113c91e365644b7403a52b
Angélique Coune <angelique.coune@hotmail.com>
2021-03-17 10:27:24 +0100

    Ajout de difficultés et jeu, modififications d'autres fichiers

---
+++ app/src/main/java/com/example/colormemory/layout_difficultes.java
@@ -0,0 +1,30 @@
+package com.example.colormemory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class layout_difficultes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_difficultes);
+    }
+
+    //Interface mode facile
+
+    //Interface mode difficile
+
+    //Interface mode expert
+
+    //Interface mode chrono
+
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/colormemory/layout_jeu.java
+++ app/src/main/java/com/example/colormemory/layout_jeu.java
@@ -2,13 +2,13 @@ package com.example.colormemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class layout_activity extends AppCompatActivity {
+public class layout_jeu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_activite_principale);
+        setContentView(R.layout.layout_jeu);
     }
-}
