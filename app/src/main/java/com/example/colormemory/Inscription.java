4932b6943595d68b956503828478cddd0569cdce
Angélique Coune <angelique.coune@hotmail.com>
2021-03-21 15:57:04 +0100

    Navigation entre les pages

--- app/src/main/java/com/example/colormemory/Inscription.java
+++ app/src/main/java/com/example/colormemory/Inscription.java
@@ -1,31 +1,30 @@
 package com.example.colormemory;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Inscription extends AppCompatActivity {
 
-    private Button button_inscription;
+    private Button button_b_retour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        button_inscription = findViewById(R.id.button_inscription);
-        button_inscription.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.layout_inscription);
+        button_b_retour = findViewById(R.id.button_b_retour);
+        button_b_retour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openInscription();
+                openMainActivity();
             }
         });
     }
-
-    public void openInscription(){
-        Intent intent = new Intent (this, Inscription.class);
+    public void openMainActivity(){
+        Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);
     }
 }
